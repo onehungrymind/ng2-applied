c3709567174aa82fd521f58e9da7d958278ed024
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-11-16 16:25:29 -0700

    removing items from app

--- src/app/widgets/widgets-list/widgets-list.component.spec.ts
+++ src/app/widgets/widgets-list/widgets-list.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { WidgetsListComponent } from './widgets-list.component';
 
-describe('Component: ItemsList', () => {
+describe('Component: WidgetsList', () => {
   it('should create an instance', () => {
     let component = new WidgetsListComponent();
     expect(component).toBeTruthy();
